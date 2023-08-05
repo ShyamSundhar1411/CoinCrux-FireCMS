33f709c0e5d058b8c5835fa69911c081788a92af
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2023-08-05 20:45:41 +0530

    Deployment

---
+++ src/firebase-config.ts
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyC2NODVa6qR1v-Rg4zEw3kyRtHgW2rOWcI",
+  authDomain: "coin-crux.firebaseapp.com",
+  projectId: "coin-crux",
+  storageBucket: "coin-crux.appspot.com",
+  messagingSenderId: "427053135616",
+  appId: "1:427053135616:web:25dae857a85c6a5f7aa5e5",
+  measurementId: "G-7RPEP2NKJE"
+};
